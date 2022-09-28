ea657c01d4ca4caa57e2fbba1e33558d920d657e
zungx <vanzungx@gmail.com>
2022-09-29 01:22:43 +0700

    add vector sample

--- stl/vector.cpp
+++ stl/vector.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
-    cout << "vector";
+    vector<int> first;
+    vector<int> second (4, 100);
+    vector<int> third (second.begin(), second.end());
+
+    vector<int> v;
+    for (int i=0; i<5; i++) v.push_back(i);
+    cout << v.front() << endl;
+    cout << v.back() << endl;
+    cout << v.size() << endl;
+    v.clear();
+    cout << v.empty() << endl;
+
+    for (int i=1; i<7; i++) v.push_back(i);
+    
+    for (int i=0; i<v.size(); i++) cout << v[i] << "_";
+    cout << endl;
+
+    v.erase(v.begin() + 3); // delete element pos 2
+    for (int i=0; i<v.size(); i++) cout << v[i] << "_";
+    cout << endl;
+    
+    v.erase(v.begin(), v.begin() + 2);
+    for (int i=0; i<v.size(); i++) cout << v[i] << "_";
+    cout << endl;
+
+    v.insert(v.begin(), 100);
+    v.insert(v.end(), 20);
+
+    for(auto it = v.begin(); it != v.end(); it++) cout << *it << "_";
+    cout << endl;
+
+    for (auto it = v.rbegin(); it != v.rend(); it++) cout << *it << "_";
+    cout << endl;
+
 
     return 0;
 }
